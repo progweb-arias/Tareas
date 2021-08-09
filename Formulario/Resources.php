8a6403a2ddf6a74818a5be66e1d831f48592d5a2
Arias <progweb@orbitadigital.com>
2021-08-09 19:25:46 +0200

    Añadi comentarios de las funciones

--- Formulario/Resources.php
+++ Formulario/Resources.php
@@ -1,7 +1,15 @@
 <?php
 class Resources
 {
-    public function validate($nombre, $edad, $fecha)
+    /**
+     * Funcion validar campos
+     * @param string $nombre 
+     * @param int $edad 
+     * @param string $fecha
+     * 
+     * @return array      
+     */
+    public function validate(string $nombre, int $edad, string $fecha)
     {
         $resultados = ["correcto" => [], "error" => []];
         $cadena = array("texto" => $nombre, "numero" => $edad, "fecha" => $fecha);
@@ -14,12 +22,26 @@ class Resources
         }
         return $resultados;
     }
-    public function save_validate($nombre, $edad, $fecha)
+    /**
+     * Funcion de validar y guardar los datos en una tabla
+     * @param string $nombre
+     * @param int $edad  
+     * @param string $fecha
+     * 
+     * @return bool
+     */
+    public function save_validate(string $nombre, int $edad, string $fecha): bool
     {
         $fecha = date('Y-m-d', strtotime($fecha));
         return Db::getInstance()->execute("INSERT INTO " . _DB_PREFIX_ . "formulario(Nombre,Edad,Fecha,Fecha_creacion,Fecha_Modificacion) VALUES('$nombre',$edad,'$fecha',NOW(),NOW())");
     }
-    public function delete($nombre, $edad, $fecha)
+    /**
+     * Funcion de borrar en la tabla
+     * @param string $nombre
+     * 
+     * @return bool
+     */
+    public function delete(string $nombre): bool
     {
         return Db::getInstance()->execute("DELETE FROM " . _DB_PREFIX_ . "formulario WHERE Nombre='$nombre'");
     }
