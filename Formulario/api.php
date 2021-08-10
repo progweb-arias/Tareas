2464b99961805bbe2f1ef47c28febc962f3e37bc
Arias <progweb@orbitadigital.com>
2021-08-10 16:34:22 +0200

    Correciones en el caso validate

--- Formulario/api.php
+++ Formulario/api.php
@@ -8,14 +8,14 @@ if (!defined('_PS_VERSION_')) {
 // Requerir acceso al Resources.php para poder acceder a las funciones
 require_once 'Resources.php';
 //Obtener el valor action del metodo GET
-$action = Tools::getValue('action');
+$action = Tools::getValue('action', '');
 //Entrar en el switch con el valor del action obtenido anteriormente y realizar las distintas llamadas en funcion 
 //dicho valor
 switch ($action) {
         // Para el caso validate
     case 'validate':
         $resources = new Resources();
-        $resultados = $resources->validate($_POST['texto'], (int)$_POST['numero'], $_POST['fecha']);
+        $resultados = $resources->validate(Tools::getValue('texto', ''), (int)Tools::getValue('numero', ''), Tools::getValue('fecha', ''));
         die(json_encode($resultados));
         break;
         // Para el caso save_validate
