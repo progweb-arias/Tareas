8fe6247ed8cc6575bd30867c7a355fffa290790e
Arias <progweb@orbitadigital.com>
2021-08-11 13:07:54 +0200

    Update table fields

--- Formulario/install.php
+++ Formulario/install.php
@@ -5,7 +5,7 @@ if (!defined('_PS_VERSION_')) {
 }
 
 
-if (Db::getInstance()->execute("CREATE TABLE IF NOT EXISTS " . _DB_PREFIX_ . "formulario(id int auto_increment,Nombre Varchar(20) NOT NULL,Edad int NOT NULL,Fecha date NOT NULL,Fecha_creacion datetime NOT NULL,Fecha_Modificacion datetime NOT NULL,PRIMARY KEY(ID))")) {
+if (Db::getInstance()->execute("CREATE TABLE IF NOT EXISTS " . _DB_PREFIX_ . "formulario2(id int auto_increment,Nombre Varchar(20) NOT NULL,Edad int NOT NULL,Fecha date NOT NULL,Fecha_creacion datetime NOT NULL,Fecha_Modificacion datetime NOT NULL,deleted bool NOT NULL,date_deleted datetime NULL,PRIMARY KEY(ID))")) {
     echo "Se crea correctamente";
 } else {
     echo "No se creo. Error desconocido";
