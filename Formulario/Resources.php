9efa9a412168d93fe81b8022377357c824d79525
Arias <progweb@orbitadigital.com>
2021-08-18 16:50:28 +0200

    Correct function Update in Resources

--- Formulario/Resources.php
+++ Formulario/Resources.php
@@ -100,7 +100,13 @@ class Resources
     }
     public function update(string $nombre, int $edad, string $fecha, string $fecha_desde)
     {
-        // echo ("UPDATE " . _DB_PREFIX_ . "formulario SET Nombre='$nombre', Edad=$edad, Fecha='$fecha' WHERE Fecha_creacion= $fecha_desde");
+        $array = array("Nombre" => $nombre, "Edad" => $edad, "Fecha" => $fecha);
+        foreach ($array as $variables) {
+            if (!$variables) {
+                return $array;
+            }
+        }
         return Db::getInstance()->execute("UPDATE " . _DB_PREFIX_ . "formulario SET Nombre='$nombre', Edad=$edad, Fecha='$fecha' WHERE Fecha_creacion= '$fecha_desde'");
+        // echo ("UPDATE " . _DB_PREFIX_ . "formulario SET Nombre='$nombre', Edad=$edad, Fecha='$fecha' WHERE Fecha_creacion= $fecha_desde");
     }
 }
