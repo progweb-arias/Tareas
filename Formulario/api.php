8eb1fd3dc2812e18fc7780ccd002205d85d4ef6b
Arias <progweb@orbitadigital.com>
2021-08-19 16:47:01 +0200

    correct api for pagination in table

--- Formulario/api.php
+++ Formulario/api.php
@@ -39,22 +39,10 @@ switch ($action) {
         // en caso de que no entren en ningun caso por no establecer bien el action
         // Para el caso showTable
     case 'search':
-        $resultados = $resources->search(Tools::getValue('texto', ''), Tools::getValue('fecha_desde', ''), Tools::getValue('fecha_hasta', ''), Tools::getValue('boton', ''));
+        $resultados = $resources->search(Tools::getValue('texto', ''), Tools::getValue('fecha_desde', ''), Tools::getValue('fecha_hasta', ''), Tools::getValue('boton', ''), Tools::getValue('boton1', ''));
         break;
     case 'update':
         $resultados = $resources->update(Tools::getValue('texto', ''), (int)Tools::getValue('numero', 0), Tools::getValue('fecha', ''), Tools::getValue('fecha_desde', ''));
-        break;
-    case 'boton1':
-        $resultados = $resources->boton1((int)Tools::getValue('boton1', 0));
-        break;
-    case 'boton2':
-        $resultados = $resources->boton2((int)Tools::getValue('boton2', 0));
-        break;
-    case 'boton3':
-        $resultados = $resources->boton3((int)Tools::getValue('boton3', 0));
-        break;
-    case 'boton4':
-        $resultados = $resources->boton4((int)Tools::getValue('boton4', 0));
         break;
     default:
         // devuelve un string
